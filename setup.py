da10b5462bb71d4893383658d01a3344c80d4591
Ross McFarland <rwmcfa1@gmail.com>
2022-01-25 09:00:46 -0800

    Switch to pytest, update python version matrix, use latest tmpl changes

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 def descriptions():
@@ -25,15 +25,18 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     name='octodns-dnsimple',
-    packages=('octodns_dnsimple',),
+    packages=find_packages(),
     python_requires='>=3.6',
-    install_requires=('octodns>=0.9.14', 'requests>=2.26.0'),
+    install_requires=(
+        'octodns>=0.9.14',
+        'requests>=2.26.0',
+    ),
     url='https://github.com/octodns/octodns-dnsimple',
     version=version(),
     tests_require=[
         'mock>=4.0.3',
-        'nose',
-        'nose-no-network',
+        'pytest',
+        'pytest-network',
         'requests_mock'
     ],
 )
